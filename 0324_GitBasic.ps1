fc23ee0245bf230f5e7ed45ddee357ed6a4a6e30
myhyunn <myhyun1123@dgu.ac.kr>
2025-03-26 15:38:02 +0900

    A

---
+++ 0324_GitBasic.ps1
@@ -0,0 +1,108 @@
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git commit -m "Add README.md by MYH"
+[main d97bfc8] Add README.md by MYH
+ 1 file changed, 2 insertions(+), 1 deletion(-)
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git push origin main
+info: please complete authentication in your browser...
+Enumerating objects: 5, done.
+Counting objects: 100% (5/5), done.
+Delta compression using up to 22 threads
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (3/3), 357 bytes | 178.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+To https://github.com/myhyunn/OSSPrac.git
+   cc7a087..d97bfc8  main -> main
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git add README.md
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git commit -m "Add READMD.md by MYH"
+[main 4fc18bc] Add READMD.md by MYH
+ 1 file changed, 3 insertions(+), 3 deletions(-)
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git push origin main
+Enumerating objects: 5, done.
+Counting objects: 100% (5/5), done.
+Delta compression using up to 22 threads
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (3/3), 361 bytes | 180.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+To https://github.com/myhyunn/OSSPrac.git
+   d97bfc8..4fc18bc  main -> main
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git pull https://github.com/myhyunn/OSSPrac.git
+From https://github.com/myhyunn/OSSPrac
+Counting objects: 100% (5/5), done.
+Delta compression using up to 22 threads
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (3/3), 361 bytes | 180.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+To https://github.com/myhyunn/OSSPrac.git
+   d97bfc8..4fc18bc  main -> main
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git pull https://github.com/myhyunn/OSSPrac.git
+From https://github.com/myhyunn/OSSPrac
+Delta compression using up to 22 threads
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (3/3), 361 bytes | 180.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+To https://github.com/myhyunn/OSSPrac.git
+   d97bfc8..4fc18bc  main -> main
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git pull https://github.com/myhyunn/OSSPrac.git
+From https://github.com/myhyunn/OSSPrac
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (3/3), 361 bytes | 180.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+To https://github.com/myhyunn/OSSPrac.git
+   d97bfc8..4fc18bc  main -> main
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git pull https://github.com/myhyunn/OSSPrac.git
+From https://github.com/myhyunn/OSSPrac
+To https://github.com/myhyunn/OSSPrac.git
+   d97bfc8..4fc18bc  main -> main
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git pull https://github.com/myhyunn/OSSPrac.git
+From https://github.com/myhyunn/OSSPrac
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git pull https://github.com/myhyunn/OSSPrac.git
+From https://github.com/myhyunn/OSSPrac
+ * branch            HEAD       -> FETCH_HEAD
+Already up to date.
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git push origin main
+Everything up-to-date
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git add .
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git commit -m "Modify README.md"
+[main 785883d] Modify README.md
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git push origin main
+Everything up-to-date
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git add .
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git commit -m "Modify README.md"
+[main 785883d] Modify README.md
+Everything up-to-date
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git add .
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git commit -m "Modify README.md"
+[main 785883d] Modify README.md
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git add .
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git commit -m "Modify README.md"
+[main 785883d] Modify README.md
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git commit -m "Modify README.md"
+[main 785883d] Modify README.md
+[main 785883d] Modify README.md
+ 1 file changed, 4 insertions(+), 1 deletion(-)
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git fetch origin main
+remote: Enumerating objects: 5, done.
+remote: Counting objects: 100% (5/5), done.
+remote: Compressing objects: 100% (2/2), done.
+remote: Total 3 (delta 1), reused 0 (delta 0), pack-reused 0 (from 0)
+Unpacking objects: 100% (3/3), 961 bytes | 40.00 KiB/s, done.
+From https://github.com/myhyunn/OSSPrac
+ * branch            main       -> FETCH_HEAD
+   4fc18bc..bd40f2b  main       -> origin/main
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git merge origin main
+Auto-merging README.md
+CONFLICT (content): Merge conflict in README.md
+Automatic merge failed; fix conflicts and then commit the result.
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git add .
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git commit -m "Merge README.md"
+[main a63c3b5] Merge README.md
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> git push origin main
+Enumerating objects: 10, done.
+Counting objects: 100% (10/10), done.
+Delta compression using up to 22 threads
+Compressing objects: 100% (4/4), done.
+Writing objects: 100% (6/6), 566 bytes | 566.00 KiB/s, done.
+Total 6 (delta 2), reused 0 (delta 0), pack-reused 0 (from 0)
+remote: Resolving deltas: 100% (2/2), completed with 1 local object.
+To https://github.com/myhyunn/OSSPrac.git
+   bd40f2b..a63c3b5  main -> main
+PS C:\Users\myhyu\OneDrive - dgu.ac.kr\2025-1 OSSPrac\prac4\OSSPrac> 
